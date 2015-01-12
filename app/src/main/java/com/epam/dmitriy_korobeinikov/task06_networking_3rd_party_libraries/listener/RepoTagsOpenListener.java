bf1e59989fd4c0ccf5896eaf20a03128581b78a5
JohnKuper <bulletstorm360@gmail.com>
2015-01-12 20:03:52 +0400

    Fixed broken commit with 2 lost files.

---
+++ app/src/main/java/com/epam/dmitriy_korobeinikov/task06_networking_3rd_party_libraries/listener/RepoTagsOpenListener.java
@@ -0,0 +1,8 @@
+package com.epam.dmitriy_korobeinikov.task06_networking_3rd_party_libraries.listener;
+
+/**
+ * Created by Dmitriy Korobeynikov on 10.01.2015.
+ */
+public interface RepoTagsOpenListener {
+    public void openRepoTags(int repositoryId);
+}
